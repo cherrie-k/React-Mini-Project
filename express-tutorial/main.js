6b10d6481c4db58c028b41335ef868b46036f438
Cherrie <cherriekim925@gmail.com>
2023-02-01 04:56:15 +0900

    added user-defined middleware

--- express-tutorial/main.js
+++ express-tutorial/main.js
@@ -2,6 +2,16 @@ var express = require("express");
 var app = express();
 var user = require("./routes/user");
 
+/* 함수를 만들어서 app.use에 넣어준다. 
+request가 들어 왔을 때 다른걸 실행하기 전에 myLogger가 실행되어 처리할 작업을 하고(console.log어쩌구), 
+거기서 받은 next를 실행하면 다른 미들웨어를 실행하거나, 아니면 라우팅 작업 실행...*/
+var myLogger = function (req, res, next) {
+  console.log(req.url);
+  next();
+};
+
+app.use(myLogger);
+
 // 루트 경로에 get 요청이 들어오면 콜백 함수 실행. 파라미터는 request, response.
 app.get("/", function (req, res) {
   res.send("Hello World");
