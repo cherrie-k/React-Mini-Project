a81657f4c1d5ccae9f27662a87455e40d2dc9c29
Cherrie <cherriekim925@gmail.com>
2023-02-02 18:56:14 +0900

    added a feature to show active nav tab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,14 +8,24 @@ function NavBar() {
         Cherrie's
       </a>
       <ul>
-        <li className="active">
-          <a href="/contacts">Contacts</a>
-        </li>
-        <li>
-          <a href="/about">About</a>
-        </li>
+        <CustomLink href="/contacts">Contacts</CustomLink>
+        <CustomLink href="/about">About</CustomLink>
       </ul>
     </nav>
+  );
+}
+
+// pass href, children, and custop props
+function CustomLink({ href, children, ...props }) {
+  // active page를 나타내주는 path
+  const path = window.location.pathname;
+  return (
+    // li의 className은 path와 href가 같다면, 즉 지금 있는 곳이 active path 라면, 'active'가 된다. 저 둘이 같지 않다면, className은 그냥 ''.
+    <li className={path === href ? "active" : ""}>
+      <a href={href} {...props}>
+        {children}
+      </a>
+    </li>
   );
 }
 
